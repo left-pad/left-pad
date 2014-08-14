0b1d01e953d5136c922c7aef809a12b0bc53c2ed
E.Azer Koçulu <azer@kodfabrik.com>
2014-08-13 20:32:13 -0700

    allow custom char

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 module.exports = leftpad;
 
-function leftpad (str, len) {
+function leftpad (str, len, ch) {
   var i = -1;
+
+  ch || (ch = ' ');
   len = len - str.length;
 
   while (++i < len) {
-    str = ' ' + str;
+    str = ch + str;
   }
 
   return str;
