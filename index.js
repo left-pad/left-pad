7aa20d4289b7c706787adfcff7056f7bc0349e62
E.Azer Koçulu <azer@kodfabrik.com>
2014-08-15 00:16:00 -0700

    make sure its str

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 module.exports = leftpad;
 
 function leftpad (str, len, ch) {
+  str = String(str);
+
   var i = -1;
 
   ch || (ch = ' ');
   len = len - str.length;
 
-  str = String(str);
 
   while (++i < len) {
     str = ch + str;
