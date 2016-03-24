921862021e26f9bcb39feebf6ff2e849b179017b
Sagiv Ofek <sagivo@users.noreply.github.com>
2016-03-24 00:14:32 -0400

    Improve runtime efficiency
    using array instead of strings to concatenate the response.

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ module.exports = leftpad;
 
 function leftpad (str, len, ch) {
   str = String(str);
-
-  var i = -1;
-
   if (!ch && ch !== 0) ch = ' ';
-
+  
   len = len - str.length;
-
-  while (++i < len) {
-    str = ch + str;
-  }
-
-  return str;
+  if (len < 1) return str;
+  
+  var arr = new Array(len+1);
+  for(var i=0;i<arr.length;i++)
+    arr[i] = (i == arr.length - 1) ? str : ch;
+    
+  return arr.join('');
 }
