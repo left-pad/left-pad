e59e8bd60554951890e19ed866c386b0a7134076
Sagiv Ofek <sagivo@users.noreply.github.com>
2016-03-24 02:13:46 -0400

    Shorten the code and use join only once

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ module.exports = leftpad;
 
 function leftpad (str, len, ch) {
   str = String(str);
-
   len = len - str.length;
   if (len < 1) return str;
-  
   if (!ch && ch !== 0) ch = ' ';
+
   var arr = new Array(len+1);
-  for(var i=0; i<arr.length; i++)
-    arr[i] = (i == arr.length - 1) ? str : ch;
-    
-  return arr.join('');
+  arr[arr.length - 1] = str;
+  return arr.join(ch);
 }
