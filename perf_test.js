1a90e0f36a9c967731f09a9b2985ab95f5a1d06c
Hao Chen <haoel@hotmail.com>
2016-03-25 12:28:55 +0800

    performance test

---
+++ perf_test.js
@@ -0,0 +1,92 @@
+function leftpad_orginal (str, len, ch) {
+  str = String(str);
+
+  var i = -1;
+
+  if (!ch && ch !== 0) ch = ' ';
+
+  len = len - str.length;
+
+  while (++i < len) {
+    str = ch + str;
+  }
+
+  return str;
+}
+
+
+
+function leftpad_es6_repeat(str, len, ch) {
+  str = String(str);
+  if (!ch && ch !== 0) ch = ' ';
+  var l = len - str.length; 
+  if (l > 0) return ch.repeat(l)+str;
+  return str;
+}
+
+
+
+function leftpad_bit_ops(str, len, ch) {
+  str = String(str);
+
+  if (!ch && ch !== 0) ch = ' ';
+
+  len = len - str.length;
+  if (len <= 0) return str;
+
+  ch = ch + '';
+  var pad = '';
+  while (true) {
+    if (len & 1) pad += ch;
+    len >>= 1;
+    if (len) ch += ch;
+    else break;
+  }
+  return pad + str;
+}
+
+
+
+
+function test(fn, str, len, times) {
+  var expected = " ".repeat(len-str.length) + str;
+  len = expected.length;
+  console.time(fn.name); 
+  for(i=0; i<times; i++) {
+    if (expected !== fn(str, len, ' ') ){
+      console.log("error");
+      break;
+    }
+  }
+  console.timeEnd(fn.name); 
+}
+
+for(var round=0; round<5; round++) {
+  console.log("====== "+round + " ======");
+  var times = 1000000;
+  var str = "abcd"
+  var len = 100;
+  console.log("test(\""+str+"\", " + len + ")" );
+  test(leftpad_orginal, str, len, times);
+  test(leftpad_es6_repeat, str, len, times);
+  test(leftpad_bit_ops, str, len, times);
+  console.log("\n");
+  
+  str = "abcd"
+  len = 10;
+  console.log("test(\""+str+"\", " + len + ")" );
+  test(leftpad_orginal, str, len, times);
+  test(leftpad_es6_repeat, str, len, times);
+  test(leftpad_bit_ops, str, len, times);
+  console.log("\n");
+  
+  str = "abcd"
+  len = 5;
+  console.log("test(\""+str+"\", " + len + ")" );
+  test(leftpad_orginal, str, len, times);
+  test(leftpad_es6_repeat, str, len, times);
+  test(leftpad_bit_ops, str, len, times);
+  console.log("\n");
+}
+
+
