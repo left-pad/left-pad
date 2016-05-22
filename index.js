41be73ab8cf9ba62e4ba7347bd8baa745f656b12
Bowen <bowensyd@gmail.com>
2016-05-22 16:00:16 +0200

    performance increased a bit

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ function leftPad (str, len, ch) {
   len = len - str.length;
   if (len <= 0) return str;
 
-  ch = typeof ch === "undefined"?" ":ch;
-  var pad = new Array(++len).join(ch+"");
-  return pad + str;
+  ch = typeof ch === "undefined"?" ":ch+"";
+  var pad = '';
+
+  while (true) {
+    if (len & 1) pad += ch;
+    len >>= 1;
+    if (len) {
+    	ch += ch;
+    } else {
+    	return pad + str;
+    }
+  }
 }
+
+
+
