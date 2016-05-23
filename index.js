72fc47590325c028dbabcd0463014088849bb49e
Bowen <bowensyd@gmail.com>
2016-05-23 11:37:32 +0200

    less ifs,fairly faster

--- index.js
+++ index.js
@@ -22,19 +22,14 @@ function leftPad (str, len, ch) {
   len = len - str.length;
   if (len <= 0) return str;
 
-  ch = typeof ch === "undefined"?" ":ch+"";
+  ch = typeof ch === 'undefined'?' ':ch+'';
   if (ch === ' ' && len < 10) return cache[len] + str;
   var pad = '';
-
-  while (true) {
-    if (len & 1) pad += ch;
-    len >>= 1;
-    if (len) {
-    	ch += ch;
-    } else {
-    	return pad + str;
-    }
-  }
+  do{
+  	(len & 1) && (pad += ch);
+  	len >>= 1;
+  } while (len && (ch += ch));
+  return pad + str;
 }
 
 
