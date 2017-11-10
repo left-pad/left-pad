6dcb330e6d6a241a2e5d443c7c38f12c83a0f2a9
Paito Anderson <pj.paito@gmail.com>
2017-11-09 20:44:38 -0500

    Copied Typings for Convenience
    Copied from: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/left-pad/index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,7 @@
-declare module "left-pad" {
-    export default function(string: number|string, length: number, character?: number|string): string;
-}
+// Type definitions for left-pad 1.1
+// Project: https://github.com/stevemao/left-pad
+// Definitions by: Zlatko Andonovski <https://github.com/Goldsmith42>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+declare function leftPad(str: string|number, len: number, ch?: string|number): string;
+
+export = leftPad;
