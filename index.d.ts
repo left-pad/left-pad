0a54cb47f6d297b3d9aab25040ae8f4ddee2fd3c
Zac <zacfirst@gmail.com>
2018-04-09 10:15:15 +1000

    chore: add typescript namespace declaration (#60)

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
-// Type definitions for left-pad 1.1
+// Type definitions for left-pad 1.2.0
 // Project: https://github.com/stevemao/left-pad
-// Definitions by: Zlatko Andonovski <https://github.com/Goldsmith42>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Definitions by: Zlatko Andonovski, Andrew Yang, Chandler Fang and Zac Xu
+
 declare function leftPad(str: string|number, len: number, ch?: string|number): string;
+
+declare namespace leftPad { }
 
 export = leftPad;
